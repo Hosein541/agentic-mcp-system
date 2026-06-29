90322a8fbfc0038f443c45c4601be0f35726e20a
Hosein541 <huseinmohammadi83@gmail.com>
2026-06-29 10:05:00 +0330

    edit agent.py

--- agent.py
+++ agent.py
@@ -42,45 +42,7 @@ class MCPAgent:
 
         return calls
 
-    # async def plan(self, user_input: str):
-
-    #     planner_prompt = f"""
-    # You are a planner for an AI agent.
-
-    # Break the user request into a step-by-step plan.
-
-    # You MUST output ONLY valid JSON.
-
-    # Each step must be:
-    # - tool name
-    # - arguments
-
-    # Available tools:
-    # {[t.name for t in self.tools[0].function_declarations]}
-
-    # User request:
-    # {user_input}
-
-    # Output format:
-    # {{
-    #   "steps": [
-    #     {{
-    #       "tool": "tool_name",
-    #       "args": {{
-    #         "a": 1,
-    #         "b": 2
-    #       }}
-    #     }}
-    #   ]
-    # }}
-    # """
-
-    #     response = self.llm.generate(
-    #         messages=planner_prompt,
-    #         tools=None
-    #     )
-
-    #     return json.loads(response.text)
+    
 
     async def plan(self, user_input: str):
 
@@ -122,8 +84,6 @@ class MCPAgent:
         # Gemini raw text
         raw = response.text
 
-        # validate with Pydantic
-        # plan = Plan.model_validate_json(raw)
 
         return json.loads(raw)
     
@@ -148,32 +108,6 @@ class MCPAgent:
 
         return results
 
-    # async def execute_plan(self, plan: Plan):
-
-    #     results = []
-
-    #     for step in plan.steps:
-
-    #         try:
-    #             result = await self.mcp.call_tool(
-    #                 step.tool,
-    #                 step.args
-    #             )
-
-    #             results.append({
-    #                 "tool": step.tool,
-    #                 "args": step.args,
-    #                 "result": result
-    #             })
-
-    #         except Exception as e:
-
-    #             results.append({
-    #                 "tool": step.tool,
-    #                 "error": str(e)
-    #             })
-
-    #     return results
     
 
     async def finalize(self, user_input, results):
@@ -197,23 +131,7 @@ class MCPAgent:
 
         return response.text
 
-    # async def chat(self, user_input: str):
-        
 
-    #     # STEP 1: PLAN
-    #     plan = await self.plan(user_input)
-
-    #     print("PLAN:", plan)
-
-    #     # STEP 2: EXECUTE
-    #     results = await self.execute_plan(plan)
-
-    #     print("RESULTS:", results)
-
-    #     # STEP 3: FINALIZE
-    #     answer = await self.finalize(user_input, results)
-
-    #     return answer
     
     async def chat(self, user_input: str):
 
@@ -230,78 +148,10 @@ class MCPAgent:
         final_answer = await self.finalize(user_input, results)
 
 
-        # 4. build final prompt
-    #     final_prompt = f"""
-    # User asked:
-    # {user_input}
 
-    # Execution results:
-    # {results}
-
-    # Give final answer.
-    # """
-
-    #     response = self.llm.generate(
-    #         contents=final_prompt,
-    #         tools=None
-    #     )
-
-    #     final_answer = response.text
 
         # 5. save assistant output
         self.memory.add_assistant(text=final_answer)
 
         return final_answer
-        # messages = user_input
-
-        # while True:
-
-        #     response = self.llm.generate(
-        #         messages=messages,
-        #         tools=self.tools
-        #     )
-        #     resp= str(response)
-        #     print(f"response of the llm:\t{resp[:250]}")
-
-        #     function_calls = self._extract_function_calls(response)
-
-        #     # اگر مدل پاسخ نهایی داد
-        #     if not function_calls:
-
-        #         return response.text
-
-        #     # اگر tool call داشت
-        #     tool_results = []
-
-        #     for call in function_calls:
-
-        #         result = await self.mcp.call_tool(
-        #             call.name,
-        #             dict(call.args)
-        #         )
-        #         res = str(result)
-        #         print(f"result of the tool call:\t{res[:250]}")
-
-        #         tool_results.append(
-        #             types.Content(
-        #                 role="tool",
-        #                 parts=[
-        #                     types.Part.from_function_response(
-        #                         name=call.name,
-        #                         response={
-        #                             "result": result
-        #                         }
-        #                     )
-        #                 ]
-        #             )
-        #         )
-
-        #     # ساخت context جدید
-        #     messages = [
-        #         types.Content(
-        #             role="user",
-        #             parts=[types.Part.from_text(text=user_input)]
-        #         ),
-        #         response.candidates[0].content,
-        #         *tool_results
-        #     ]
+        
